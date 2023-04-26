e464d561aea319b9f85e1e0d0635fd3034db9959
YOIS <sioy23@gmail.com>
2023-04-25 23:04:21 -0300

    add property type model

---
+++ app/app/Http/Controllers/AbPropertyTypeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAbPropertyTypeRequest;
+use App\Http\Requests\UpdateAbPropertyTypeRequest;
+use App\Models\AbPropertyType;
+
+class AbPropertyTypeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAbPropertyTypeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AbPropertyType $abPropertyType)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AbPropertyType $abPropertyType)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAbPropertyTypeRequest $request, AbPropertyType $abPropertyType)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AbPropertyType $abPropertyType)
+    {
+        //
+    }
+}
