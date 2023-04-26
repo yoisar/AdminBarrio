8d73cef40014bbd56bcfe02bf26674fbd7dd813d
YOIS <sioy23@gmail.com>
2023-04-25 23:01:07 -0300

    ref country model

--- app/app/Models/AbCountry.php
+++ app/app/Models/AbCountry.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class AbCountry extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'iso_code'
+    ];
 }
--- app/database/seeders/AbCountrySeeder.php
+++ app/database/seeders/AbCountrySeeder.php
@@ -4,14 +4,17 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class AbCountrySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        DB::table('ab_countries')->insert([
+            ['name' => 'Argentina', 'iso_code' => 'AR'],
+        ]);
     }
 }
