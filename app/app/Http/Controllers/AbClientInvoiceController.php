69a69118101fc9474ad454d4c5291c39baf246a9
YOIS <sioy23@gmail.com>
2023-05-05 00:44:18 -0300

    add client invoice model

---
+++ app/app/Http/Controllers/AbClientInvoiceController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAbClientInvoiceRequest;
+use App\Http\Requests\UpdateAbClientInvoiceRequest;
+use App\Models\AbClientInvoice;
+
+class AbClientInvoiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAbClientInvoiceRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AbClientInvoice $abClientInvoice)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AbClientInvoice $abClientInvoice)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAbClientInvoiceRequest $request, AbClientInvoice $abClientInvoice)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AbClientInvoice $abClientInvoice)
+    {
+        //
+    }
+}
