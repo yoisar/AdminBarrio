99635100abf7b2e77e1bb0674f9368cbd63bba14
YOIS <sioy23@gmail.com>
2023-05-05 00:41:32 -0300

    add client model

---
+++ app/app/Http/Controllers/AbClientController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAbClientRequest;
+use App\Http\Requests\UpdateAbClientRequest;
+use App\Models\AbClient;
+
+class AbClientController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAbClientRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AbClient $abClient)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AbClient $abClient)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAbClientRequest $request, AbClient $abClient)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AbClient $abClient)
+    {
+        //
+    }
+}
