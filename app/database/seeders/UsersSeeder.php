e201f053d818202b57e182009f59242ba293390e
YOIS <sioy23@gmail.com>
2023-05-07 14:10:46 -0300

    ref users seeder

--- app/database/seeders/UsersSeeder.php
+++ app/database/seeders/UsersSeeder.php
@@ -16,17 +16,61 @@ class UsersSeeder extends Seeder
      */
     public function run(Generator $faker)
     {
+        // system user
         $demoUser = User::create([
-            'name'              => 'Adminstrador Barrio',
-            'email'             => 'admin.barrioa@gmail.com',
-            'password'          => Hash::make('demo'),
+            'name'              => 'System User',
+            'email'             => 'admin.systema@adminbarrio.com',
+            'password'          => Hash::make('@dm1nBarr10.2023'),
             'email_verified_at' => now(),
         ]);
-
+        // super admin user
         $demoUser2 = User::create([
             'name'              => 'Yassel Omar Izquierdo Souchay',
             'email'             => 'sioy23@gmail.com',
             'password'          => Hash::make('y01s'),
+            'email_verified_at' => now(),
+        ]);
+        //  Administrador de barrios
+        $demoUser = User::create([
+            'name'              => 'Administrador de Barrios',
+            'email'             => 'admin.barrios@adminbarrio.com',
+            'password'          => Hash::make('demo'),
+            'email_verified_at' => now(),
+        ]);
+
+        // Administrador de alquileres
+        $demoUser3 = User::create([
+            'name'              => 'Administrador de Alquieres',
+            'email'             => 'admin.alquileres@adminbarrio.com',
+            'password'          => Hash::make('gu1ll3'),
+            'email_verified_at' => now(),
+        ]);
+        // Administrador Barrios y Alquileres
+        $demoUser3 = User::create([
+            'name'              => 'Administrador Barrios y Alquileres',
+            'email'             => 'admin.barrio.alquileres@adminbarrio.com',
+            'password'          => Hash::make('gu1ll3'),
+            'email_verified_at' => now(),
+        ]);
+        // Residente propietario de Barrio
+        $demoUser3 = User::create([
+            'name'              => 'Residente Propierario',
+            'email'             => 'residente.propietario.barrio@adminbarrio.com',
+            'password'          => Hash::make('gu1ll3'),
+            'email_verified_at' => now(),
+        ]);
+        // Residente de Inquilino de barrio
+        $demoUser3 = User::create([
+            'name'              => 'Residente de Barrio',
+            'email'             => 'residente.inquilino.barrioo@adminbarrio.com',
+            'password'          => Hash::make('gu1ll3'),
+            'email_verified_at' => now(),
+        ]);
+        // Residente de inquilino Alquiler
+        $demoUser3 = User::create([
+            'name'              => 'Residente de Alquier',
+            'email'             => 'residente.inquilino.alquiler@adminbarrio.com',
+            'password'          => Hash::make('gu1ll3'),
             'email_verified_at' => now(),
         ]);
     }
