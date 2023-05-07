f66d3f25803bd4d6cb6dfc09cc9cca443d47741e
YOIS <sioy23@gmail.com>
2023-05-07 16:30:46 -0300

    ref invoice model

--- app/app/Models/AbClientInvoice.php
+++ app/app/Models/AbClientInvoice.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class AbClientInvoice extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'ab_client_id', // relación con el modelo Client
+        'invoice_number',
+        'invoice_date',
+        'due_date',
+        'amount',
+        'status', // puede ser 'draft', 'sent', 'paid', 'pending',  'overdue'
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
 }
--- app/database/migrations/2023_05_05_033950_create_ab_client_invoices_table.php
+++ app/database/migrations/2023_05_05_033950_create_ab_client_invoices_table.php
@@ -12,11 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('ab_client_invoices', function (Blueprint $table) {
-            $table->id();
+            $table->id();            
             $table->unsignedBigInteger('ab_client_id');
-            $table->date('date');
+            $table->string('invoice_number');
+            $table->date('invoice_date');
+            $table->date('due_date');
             $table->decimal('amount', 10, 2);
-            $table->timestamps();
+            $table->string('status')->nullable();
+            $table->timestamps();            
             $table->foreign('ab_client_id')->references('id')->on('ab_clients')->onDelete('cascade');
         });
     }
