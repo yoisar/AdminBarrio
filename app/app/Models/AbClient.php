ca7b2a19ab19c4e31bd58549219ada2f52f34213
YOIS <sioy23@gmail.com>
2023-05-07 18:39:21 -0300

    update client model  fillable fields

--- app/app/Models/AbClient.php
+++ app/app/Models/AbClient.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class AbClient extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'full_name',
+        'document_type',
+        'document_number',
+        'nationality',
+        'address',
+        'phone',
+        'billing_name',
+        'billing_address',
+        'tax_id',
+        'tax_responsibility'
+    ];
 }
